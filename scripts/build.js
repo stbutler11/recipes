3da4938d230b9a7a57e03811a2fdf163f3b7c247
Stuart Butler <stbutler11@gmail.com>
2021-12-10 16:21:42 +0000

    Added start of build script

---
+++ scripts/build.js
@@ -0,0 +1,7 @@
+const pug = require('pug');
+
+// Compile template.pug, and render a set of data
+console.log(pug.renderFile('template.pug', {
+  name: 'Timothy'
+}));
+// "<p>Timothy's Pug source code!</p>"
